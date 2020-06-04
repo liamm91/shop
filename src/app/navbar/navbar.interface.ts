84252bbb616514b606aad1e6aa46e8857eb99308
Liam Major <liamm91@kprschools.ca>
2020-06-04 01:36:55 -0400

    made a file to list all the links on the website along with an interface to help with properties

---
+++ src/app/navbar/navbar.interface.ts
@@ -0,0 +1,4 @@
+export interface Navlink {
+  routerLink: string;
+  linkText: string;
+}
---
+++ src/app/navbar/navbar.links.ts
@@ -0,0 +1,11 @@
+import { Navlink } from './navbar.interface';
+
+export const LINKS: Navlink[] = [
+  {routerLink: '/sale', linkText: 'On Sale'},
+  {routerLink: '/new', linkText: 'New'},
+  {routerLink: '/livingroom', linkText: 'Living Room'},
+  {routerLink: '/diningroom', linkText: 'Dining Room'},
+  {routerLink: '/office', linkText: 'Office'},
+  {routerLink: '/kitchen', linkText: 'Kitchen'},
+  {routerLink: '/decorative', linkText: 'Decorative'},
+];
