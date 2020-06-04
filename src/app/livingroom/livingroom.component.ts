95a0a86d0d54077c325be3f6a88d544330f10a2e
Liam Major <liamm91@kprschools.ca>
2020-06-04 01:41:12 -0400

    added a new 'livingroom' component

---
+++ src/app/livingroom/livingroom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-livingroom',
+  templateUrl: './livingroom.component.html',
+  styleUrls: ['./livingroom.component.css']
+})
+export class LivingroomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
