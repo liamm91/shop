ba3243c82a988cf102ad826652c171af83d5f983
Liam Major <liamm91@kprschools.ca>
2020-06-04 01:59:20 -0400

    dynamically added the routing to all the new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LINKS } from './navbar/navbar.links';
 
-const routes: Routes = [
-  //{path: '', component: }
-];
+const routes: Routes = [];
+
+// format: {path: '', component: componentName}
+LINKS.forEach(obj => routes.push({path: `${obj.routerLink}`, component: obj.component}));
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
