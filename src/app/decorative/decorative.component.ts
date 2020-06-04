5c60d975cec79ecb7f77dc7487b4eb60881707f3
Liam Major <liamm91@kprschools.ca>
2020-06-04 01:43:05 -0400

    added a new 'decorative' component

---
+++ src/app/decorative/decorative.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-decorative',
+  templateUrl: './decorative.component.html',
+  styleUrls: ['./decorative.component.css']
+})
+export class DecorativeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
