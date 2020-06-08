3cc512b909a94593749261085f2ced65050cfc5e
liamm91 <liamm91@kprschools.ca>
2020-06-07 23:06:33 -0400

    commented out all mongodb code

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from "rxjs";
-const {MongoClient} = require('mongodb').MongoClient; 
+//const { MongoClient } = require('mongodb').MongoClient; 
 
-const uri = "mongodb://localhost:27017/?readPreference=primary&ssl=false"
-const client = new MongoClient(uri)
-const dbName = "shop"
+// workaround bc angular is quite intelligent
+//declare var require: any;
+
+// const uri = "mongodb://localhost:27017/?readPreference=primary&ssl=false"
+// const client = new MongoClient(uri)
+// const dbName = "shop"
 
 @Injectable({
   providedIn: 'root'
@@ -13,19 +16,19 @@ export class ProductsService {
 
   constructor() { }
 
-  async fetchProducts(): Promise<any> {
-    // connecting to the database
-    client.connect(function(err, client) {
-      // if there is an error, return false
-      if (err) {
-        return of(err)
-      } else {
-        console.log("Connected to the database successfully!")
+  // async fetchProducts(): Promise<any> {
+  //   // connecting to the database
+  //   client.connect(function(err, client) {
+  //     // if there is an error, return false
+  //     if (err) {
+  //       return of(err)
+  //     } else {
+  //       console.log("Connected to the database successfully!")
 
-        // querying the database
-        const db = client.db(dbName)
-        return (db.collection("products").find({}))
-      }
-    })
-  }
+  //       // querying the database
+  //       const db = client.db(dbName)
+  //       return (db.collection("products").find({}))
+  //     }
+  //   })
+  // }
 }
