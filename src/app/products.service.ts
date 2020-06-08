31be2c33d4435b96475927016738ec5d84bd6789
liamm91 <liamm91@kprschools.ca>
2020-06-08 12:42:28 -0400

    removing the direct connection from angular to mongo

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,10 +1,4 @@
 import { Injectable } from '@angular/core';
-// import { Observable, of } from "rxjs";
-// import { MongoClient } from "mongodb";
-
-// const uri = "mongodb://localhost:27017/?readPreference=primary&ssl=false"
-// const client = new MongoClient(uri)
-// const dbName = "shop"
 
 @Injectable({
   providedIn: 'root'
