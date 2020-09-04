57a9f03fcfa0b78c5786d47303112b5975650f90
liamm91 <liamm91@kprschools.ca>
2020-09-04 18:04:38 -0400

    adding new router option which deals with queries to the db

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,13 +1,69 @@
 var express = require('express');
 var router = express.Router();
 const Product = require('../models/product-template')
+const legalSrch = [
+    "office",
+    "decorative",
+    "on-sale",
+    "new",
+    "living-room",
+    "dining-room",
+    "kitchen",
+];
+const regex = /[[a-z]+|(?=[a-z]*[-])([a-z-]+)]/gm
 
 /**
- * Sending a query to the database to retrieve one object
+ * Sending a query to the database to retrieve objects sorted
+ * by catagories
  */
-router.get('/product', function(req, res, next) {
-    console.log('respond with a single product');
-    Product.findOne((err, products) => {
+router.get('/query/:srch', function(req, res, next) {
+    // getting the search param from the url
+    var query = req.params.srch;
+
+    // testing to make sure its all letters
+    query.match(regex) ? {} : (res.sendStatus(404), res.end());
+    verbose.print(query.match(regex));
+
+    // testing the query against the list of legal queries
+    if (legalSrch.includes(query.match(regex)[0])) { // GOOD
+        console.log(`responded with all product of catagory ${query}`);
+        
+        res.writeHead(200, {
+            "Content-Type": "application/json", 
+            "Access-Control-Allow-Origin":"*",
+            "Access-Control-Allow-Headers":"Content-Type",
+            "Access-Control-Allow-Methods":"GET"
+        })
+    } else {
+        verbose.print("bad srch, sending 404")
+        res.sendStatus(404);
+        next()
+    }
+
+    // console.log(`responded with all product of catagory ${cata}`);
+    // Product.find((err, products) => {
+    //     if (err) {
+    //         res.writeHead(500, err)
+    //         console.log(err)
+    //     } else {
+    //         // have to write headers for CORS
+    //         res.writeHead(200, {
+    //             "Content-Type": "application/json", 
+    //             "Access-Control-Allow-Origin":"*",
+    //             "Access-Control-Allow-Headers":"Content-Type",
+    //             "Access-Control-Allow-Methods":"GET"
+    //         })
+    //         res.end(JSON.stringify(products))
+    //     }
+    // });
+});
+
+/**
+ * Sending a query to the database to retrieve all objects
+ */
+router.get('/all', function(req, res, next) {
+    console.log('responded with all products');
+    Product.find((err, products) => {
         if (err) {
             res.writeHead(500, err)
         } else {
@@ -15,28 +71,8 @@ router.get('/product', function(req, res, next) {
             res.writeHead(200, {
                 "Content-Type": "application/json", 
                 "Access-Control-Allow-Origin":"*",
-                // "Access-Control-Allow-Headers":"Content-Type",
-                // "Access-Control-Allow-Methods":"GET"
-            })
-            res.end(JSON.stringify(products))
-        }
-    });
-});
-
-/**
- * Sending a query to the database to retrieve all objects
- */
-router.get('/all', function(req, res, next) {
-    console.log('respond with all product');
-    Product.find((err, products) => {
-        if (err) {
-            res.writeHead(500, err)
-        } else {
-            res.writeHead(200, {
-                "Content-Type": "application/json", 
-                "Access-Control-Allow-Origin":"*",
-                // "Access-Control-Allow-Headers":"Content-Type",
-                // "Access-Control-Allow-Methods":"GET"
+                "Access-Control-Allow-Headers":"Content-Type",
+                "Access-Control-Allow-Methods":"GET"
             })
             res.end(JSON.stringify(products))
         }
