958fafa3e9e32f8067850e0717c3704014b923a8
liamm91 <liamm91@kprschools.ca>
2020-09-04 22:51:34 -0400

    added comments

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -6,12 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  public catagory: string;
+  public catagory: string; // property binding
 
   constructor() { }
 
   ngOnInit(): void {
-    this.catagory = "kitchen";
+    this.catagory = "kitchen"; // property binding
   }
 
 }
