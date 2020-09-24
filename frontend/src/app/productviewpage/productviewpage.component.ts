e7a84139f106d47753643dda8ad9d266fba20b7b
liamm91 <liamm91@kprschools.ca>
2020-09-23 20:34:58 -0400

    added a viewing page for selected products

---
+++ frontend/src/app/productviewpage/productviewpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productviewpage',
+  templateUrl: './productviewpage.component.html',
+  styleUrls: ['./productviewpage.component.css']
+})
+export class ProductviewpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
