6245b9f7049e941b891a5f03f488cf256ac0d670
liamm91 <liamm91@kprschools.ca>
2020-09-23 22:41:05 -0400

    implemented search by id, added output log statements

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -19,7 +19,7 @@ const regex = /(?=[a-z]*[-])([a-z-]+)|[a-z]+/gm
  * Sending a query to the database to retrieve objects sorted
  * by catagories
  */
-router.get('/query/:srch', function(req, res, next) {
+router.get('/query/:srch', function(req, res) {
     // getting the search param from the url
     var query = req.params.srch;
 
@@ -29,11 +29,16 @@ router.get('/query/:srch', function(req, res, next) {
 
     // testing the query against the list of legal queries
     if (legalSrch.includes(query.match(regex)[0])) { // GOOD
-        console.log(`responded with all product of catagory ${query}`);
         Product.find({"sys.catagory": query }, (err, products) => {
             if (err) {
+                // output 
+                console.log(`unexpected error: ${err.reason}`)
+
                 res.writeHead(500, err)
             } else {
+                // output
+                console.log(`responded with all product of catagory ${query}`);
+
                 // have to write headers for CORS
                 res.writeHead(200, {
                     "Content-Type": "application/json", 
@@ -48,14 +53,49 @@ router.get('/query/:srch', function(req, res, next) {
 });
 
 /**
- * Sending a query to the database to retrieve all objects
+ * Searching for a product by id and returning it to the page
  */
-router.get('/all', function(req, res, next) {
-    console.log('responded with all products');
-    Product.find((err, products) => {
+router.get('/id/:srch', function(req, res) {
+    // getting the search param from the url
+    var query = req.params.srch;
+
+    // searching database for product with the id specified
+    Product.find({"_id": query }, (err, products) => {
         if (err) {
+            // output 
+            console.log(`unexpected error: ${err.reason}`)
+
             res.writeHead(500, err)
         } else {
+            // output
+            console.log(`responded with product of id ${query}`);
+
+            // have to write headers for CORS
+            res.writeHead(200, {
+                "Content-Type": "application/json", 
+                "Access-Control-Allow-Origin":"*",
+                "Access-Control-Allow-Headers":"Content-Type",
+                "Access-Control-Allow-Methods":"GET"
+            })
+            res.end(JSON.stringify(products))
+        }
+    });
+});
+
+/**
+ * Sending a query to the database to retrieve all objects
+ */
+router.get('/all', function(req, res) {
+    Product.find((err, products) => {
+        if (err) {
+            // output
+            console.log(`unexpected error: ${err.reason}`);
+
+            res.writeHead(500, err)
+        } else {
+            // output
+            console.log('responded with all products');
+
             // have to write headers for CORS
             res.writeHead(200, {
                 "Content-Type": "application/json", 
