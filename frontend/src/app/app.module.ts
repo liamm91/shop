0246ed752d69741ec433d4f6c03e99d8d5bff5af
liamm91 <liamm91@kprschools.ca>
2020-09-25 20:36:03 -0400

    removed redundant components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,6 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SaleComponent } from './sale/sale.component';
-import { NewComponent } from './new/new.component';
-import { LivingroomComponent } from './livingroom/livingroom.component';
-import { DiningroomComponent } from './diningroom/diningroom.component';
-import { OfficeComponent } from './office/office.component';
-import { KitchenComponent } from './kitchen/kitchen.component';
-import { DecorativeComponent } from './decorative/decorative.component';
 import { HomeComponent } from './home/home.component';
 import { ProductboxComponent } from './productbox/productbox.component';
 import { PromotionalbarComponent } from './promotionalbar/promotionalbar.component';
@@ -26,13 +19,6 @@ import { BodyComponent } from './body/body.component';
     AppComponent,
     NavbarComponent,
     HeaderComponent,
-    SaleComponent,
-    NewComponent,
-    LivingroomComponent,
-    DiningroomComponent,
-    OfficeComponent,
-    KitchenComponent,
-    DecorativeComponent,
     HomeComponent,
     ProductboxComponent,
     PromotionalbarComponent,
